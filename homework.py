4b8e2be9f1ba8a8fbaac9f1e421ec194fb38dbee
Искандер Рыскулов <logick7@mail.ru>
2022-09-21 13:17:05 +0300

    0.3

--- homework.py
+++ homework.py
@@ -1,12 +1,13 @@
 import logging
 import os
 import sys
-import requests
-import telegram
 import time
 
+import requests
+import telegram
 from dotenv import load_dotenv
-from exceptions import EmptyKeyError, EasyError
+
+from exceptions import EasyError, EmptyKeyError
 
 load_dotenv()
 
@@ -28,7 +29,7 @@ ENDPOINT = 'https://practicum.yandex.ru/api/user_api/homework_statuses/'
 HEADERS = {'Authorization': f'OAuth {PRACTICUM_TOKEN}'}
 
 
-HOMEWORK_STATUSES = {
+HOMEWORK_VERDICTS = {
     'approved': 'Работа проверена: ревьюеру всё понравилось. Ура!',
     'reviewing': 'Работа взята на проверку ревьюером.',
     'rejected': 'Работа проверена: у ревьюера есть замечания.'
@@ -42,52 +43,48 @@ def send_message(bot, message):
         logger.info('Сообщение отправлено')
     except telegram.error.TelegramError as error:
         message = f'Ошибка при отправке сообщения {error}'
-        raise EasyError(message)
+        logger.debug(message)
 
 
 def get_api_answer(current_timestamp):
     """Запрос к эндпоинту API-сервиса."""
-    timestamp = current_timestamp or int(time.time())
+    timestamp = current_timestamp
     params = {'from_date': timestamp}
-    try:
-        response = requests.get(ENDPOINT, headers=HEADERS, params=params)
-        if response.status_code != 200:
-            raise Exception(
-                f'Неожиданный ответ сервера: {response.status_code}')
-        return response.json()
-    except requests.ConnectionError as error:
+    response = requests.get(ENDPOINT, headers=HEADERS, params=params)
+    if response.status_code != 200:
         raise Exception(
-            'Ошибка при запросе к эндпоинту API-сервиса') from error
+            f'Неожиданный ответ сервера: {response.status_code}')
+    return response.json()
 
 
 def check_response(response):
     """Проверка ответа API на корректность."""
-    if isinstance(response, dict):
-        if 'homeworks' not in response:
-            raise EmptyKeyError("Отсутствует поле 'homeworks' в ответе API")
-        homeworks = response['homeworks']
-        if 'current_date' not in response:
-            raise EmptyKeyError("Отсутствует поле 'current_date' в ответе API")
-        if isinstance(homeworks, list):
-            return homeworks
-        raise TypeError('Домашки пришли не в виде списка')
-    raise TypeError('Тип ответа API не является словарем')
+    if not isinstance(response, dict):
+        raise TypeError('Тип ответа API не является словарем')
+    if 'homeworks' not in response:
+        raise EmptyKeyError('Отсутствует поле homeworks в ответе API')
+    homeworks = response['homeworks']
+    if 'current_date' not in response:
+        raise EmptyKeyError('Отсутствует поле current_date в ответе API')
+    if isinstance(homeworks, list):
+        return homeworks
+    raise TypeError('Домашки пришли не в виде списка')
 
 
 def parse_status(homework):
     """Получение статуса домашней работы."""
     if 'homework_name' not in homework:
-        message = "Отсутствует поле 'homework_name' в ответе API"
+        message = 'Отсутствует поле homework_name в ответе API'
         raise KeyError(message)
     if 'status' not in homework:
-        message = "Отсутствует поле 'status' в ответе API"
+        message = 'Отсутствует поле status в ответе API'
         raise KeyError(message)
     homework_name = homework['homework_name']
     homework_status = homework['status']
-    if homework_status not in HOMEWORK_STATUSES:
-        message = "Неизвестный статус"
+    if homework_status not in HOMEWORK_VERDICTS:
+        message = 'Неизвестный статус'
         raise Exception(message)
-    verdict = HOMEWORK_STATUSES[homework_status]
+    verdict = HOMEWORK_VERDICTS[homework_status]
     return f'Изменился статус проверки работы "{homework_name}". {verdict}'
 
 
@@ -123,7 +120,7 @@ def main():
         except Exception as error:
             message = f'Сбой в работе программы: {error}'
             send_message(bot, message)
-
+            break
         finally:
             time.sleep(RETRY_TIME)
 
